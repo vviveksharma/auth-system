6acd28abf23206a88819931b195584b8fab75173
vviveksharma <visharma@progress.com>
2025-07-17 03:16:00 +0530

    updated the error condition
    Signed-off-by: vviveksharma <visharma@progress.com>

--- db/migrations/migrator.go
+++ db/migrations/migrator.go
@@ -2,12 +2,16 @@ package migrations
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/vviveksharma/auth/models"
 	"gorm.io/gorm"
 )
 
 func AutoMigrator(DB *gorm.DB) {
-	DB.AutoMigrate(models.DBUser{}, models.DBRoles{}, models.DBLogin{}, models.DBTenant{}, models.DBToken{}, models.DBTenantLogin{})
+	err := DB.AutoMigrate(models.DBUser{}, models.DBRoles{}, models.DBLogin{}, models.DBTenant{}, models.DBToken{}, models.DBTenantLogin{})
+	if err != nil {
+		log.Fatalln("error while migrating the tables: ", err.Error())
+	}
 	fmt.Println("Migrations done!!!")
 }
